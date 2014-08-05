3088317f59712a1e0d2f3919acf05e26052e181d
Ronglin-Kooboo <ronglin@kooboo.com>
2014-08-05 11:05:56 +0800

    update tests

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Tests
 -------------
 
 ```shell
+$ npm install
 $ npm test
 ```
 
--- test/utils.js
+++ test/utils.js
@@ -12,11 +12,21 @@ var assert = require('assert');
 
 describe('utils', function() {
     describe('#arg2arr', function() {
-        it('should return array', function() {
-
-            var arr = utils.arg2arr(arguments);
-            assert.equal(Object.prototype.toString.call(arr), '[object Array]');
-
-        });
+        var fn = function(arg1, arg2) {
+            var arr1 = utils.arg2arr(arguments);
+            var arr2 = utils.arg2arr(arguments, 1);
+            //
+            it('argument 1 should equal', function() {
+                assert.equal(arr1[0], arg1);
+            });
+            it('argument 2 should equal', function() {
+                assert.equal(arr1[1], arg2);
+            });
+            //
+            it('arguments array skip 1, then length should be 1', function() {
+                assert.equal(arr2.length, 1);
+            });
+        };
+        fn(1, 2);
     });
 });
