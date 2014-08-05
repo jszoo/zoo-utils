f69b4bac40a773b636ff96291ca3f54a8cbe1dc2
Ronglin-Kooboo <ronglin@kooboo.com>
2014-08-05 10:11:55 +0800

    update

--- test/utils.js
+++ test/utils.js
@@ -7,3 +7,16 @@
 
 'use strict';
 
+var utils = require('..');
+var assert = require('assert');
+
+describe('utils', function() {
+    describe('#arg2arr', function() {
+        it('should return array', function() {
+
+            var arr = utils.arg2arr(arguments);
+            assert.equal(Object.prototype.toString.call(arr), '[object Array]');
+
+        });
+    });
+});
